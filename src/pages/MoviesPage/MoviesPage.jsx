7bb73d58620f78460045c54e1c4e8b5b8995741a
serg444s <143083342+serg444s@users.noreply.github.com>
2024-04-07 23:01:03 +0300

    NotFoundPage

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,8 +1,23 @@
 const MoviesPage = () => {
+  const heandleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const result = form.elements.name.value;
+    console.log(result);
+    form.reset();
+  };
+
   return (
-    <div>
-      <p>MoviesPage</p>
-    </div>
+    <form onSubmit={heandleSubmit}>
+      <input
+        type="text"
+        required
+        placeholder="Search movie..."
+        autoFocus
+        name="name"
+      />
+      <button>Search</button>
+    </form>
   );
 };
 
--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const NotFoundPage = () => {
   return (
     <div>
-      <p>NotFoundPage</p>
+      <h2>Page not found</h2>
+      <p>Sorry, the requested page does not exist.</p>
+      <Link to="/">HomePage</Link>
     </div>
   );
 };

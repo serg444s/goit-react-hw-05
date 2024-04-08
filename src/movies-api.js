c07635a9c5776680655a828daf73f7d235fcaf29
serg444s <143083342+serg444s@users.noreply.github.com>
2024-04-08 14:27:45 +0300

    api

---
+++ src/movies-api.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://api.themoviedb.org";
+
+const options = {
+  headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNzA0NDFmNTUyMWY3MDQzYzAzYWNjZTRkY2E3OTZhOSIsInN1YiI6IjY2MTNhMWJhYTZhNGMxMDE4NmJkMmFlNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.O2dSmYsEn5aOiB_kpxGboL6sqJqR5uepbZpWo1LOHdw",
+  },
+};
+
+export const fetchTrendingMovies = async () => {
+  const url = "/3/trending/movie/day?language=en-US&page=1";
+  const response = await axios.get(url, options);
+  return response.data;
+};
+
+export const fetchMoviesByQuery = async (query) => {
+  const url = `/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`;
+  const response = await axios.get(url, options);
+  return response.data;
+};
+
+export const fetchMoviesById = async (id) => {
+  const url = `/3/movie/${id}?language=en-US`;
+  const response = await axios.get(url, options);
+  return response.data;
+};
+
+export const fetchCredits = async (id) => {
+  const url = `/3/movie/${id}/credits?language=en-US`;
+  const response = await axios.get(url, options);
+  return response.data;
+};
+
+export const fetchReviews = async (id) => {
+  const url = `https://api.themoviedb.org/3/movie/${id}/reviews?language=en-US&page=1`;
+  const response = await axios.get(url, options);
+  return response.data;
+};
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,17 @@
+import { fetchTrendingMovies } from "../../movies-api";
+
 const HomePage = () => {
+  const fetch = async () => {
+    try {
+      const data = await fetchTrendingMovies();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+      alert("error");
+    }
+  };
+  fetch();
+
   return (
     <div>
       <p>HomePage</p>
